0d0b61ae49f4412a572ed5267ccc41347f1225ab
naka hiro <nakamura-hiroshi01@hotmail.co.jp>
2024-02-17 00:24:33 +0900

    内容更新

---
+++ addcss.ps1
@@ -0,0 +1,15 @@
+﻿$soc = @(
+  "attendant.html"
+, "honor.html"
+, "nfhead.html"
+, "nfkyoto.html"
+, "nftokyo.html"
+, "presenter.html"
+, "staff.html", "welcomer.html"
+)
+$soc | %{
+    $html = "C:\repos\B9a2hUnacN3N\$_"
+    $data = Get-Content $html
+    $data[20] = $data[20] +"`n<link rel=`"stylesheet`" href=`"common.css`">"
+    $data | Out-File $html -Encoding UTF8
+}
