37328e7c665c7a2a7eaa66725e78d38a5366dcfd
naka hiro <nakamura-hiroshi01@hotmail.co.jp>
2024-02-18 20:00:07 +0900

    内容更新

--- addcss.ps1
+++ addcss.ps1
@@ -11,6 +11,11 @@
 $soc | %{
     $html = "C:\repos\B9a2hUnacN3N\$_"
     $data = Get-Content $html
+    for ($i = 0; $i -lt $data.Count; $i++) {
+      if ($data[$i] -match "<a href=") {
+        $data[$i] = $data[$i] -replace "<a href=", "<a target=`"_blank`" href="
+      }
+    } 
     if ($data[21] -notmatch ".*common.css.*") {
       $data[20] = $data[20] +"`n<link rel=`"stylesheet`" href=`"common.css`">"
       $data | Out-File $html -Encoding UTF8
